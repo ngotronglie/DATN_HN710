b421fa7b225e947964be6349d9aa56927ef369ab
Ngô Trọng Liêm <ngotrongliem2004@gmail.com>
2024-08-29 18:25:04 +0700

    cắt layout admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,23 @@
+@include('admin.include.head')
+
+<body>
+    @include('admin.include.aside')
+
+    <!-- Right Panel -->
+    <div id="right-panel" class="right-panel">
+        @include('admin.include.header')
+
+        @yield('content')
+
+        <div class="clearfix"></div>
+        @include('admin.include.footer')
+        <!-- /#right-panel -->
+    </div>
+
+
+
+
+    @include('admin.include.script')
+</body>
+
+</html>
---
+++ resources/views/admin/include/footer.blade.php
@@ -0,0 +1,15 @@
+<!-- Footer -->
+<footer class="site-footer">
+    <div class="footer-inner bg-white">
+        <div class="row">
+            <div class="col-sm-6">
+                Copyright &copy; 2024 by nhom HN_710
+            </div>
+            <div class="col-sm-6 text-right">
+                Designed by <a href="">Kien + Thao + Huy + My + Linh + Liem +Ban</a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- /.site-footer -->
+
