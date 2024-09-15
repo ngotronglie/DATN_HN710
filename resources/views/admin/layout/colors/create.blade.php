b7ba4ce7598a53feda5b9bff686b998d36e89236
Tên của bạn <mynttph31842@fpt.edu.vn>
2024-09-15 22:53:50 +0700

    update một chút

--- resources/views/admin/layout/colors/create.blade.php
+++ resources/views/admin/layout/colors/create.blade.php
@@ -3,21 +3,19 @@
 
 @section('content')
     <div class="content">
-        <form action="" method="post">
-            <div class="card">
-                <div class="card-header">Thêm color</div>
-                <div class="card-body card-block">
-                    <div class="form-group"><label for="company" class=" form-control-label">Company</label><input
-                            type="text" id="company" placeholder="Enter your company name" class="form-control"></div>
-                    <div class="form-group"><label for="vat" class=" form-control-label">VAT</label><input
-                            type="text" id="vat" placeholder="DE1234567890" class="form-control"></div>
-                    <div class="form-group"><label for="street" class=" form-control-label">Street</label><input
-                            type="text" id="street" placeholder="Enter street name" class="form-control"></div>
-                    <div class="row form-group">
-                    </div>
-                    <div class="form-group"><label for="country" class=" form-control-label">Country</label><input
-                            type="text" id="country" placeholder="Country name" class="form-control"></div>
-                </div>
+        <form action="{{ route(colors . create) }}" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="name">Tên màu:</label>
+                <input type="text" class="form-control" id="name" value="{{ old('name') }}">
+            </div>
+            <div class="form-group">
+                <label for="hex">Mã màu:</label>
+                <input type="color" class="form-control" id="hex" value="{{ old(color) }}">
+            </div>
+
+            <div class="form-actions form-group">
+                <button type="submit" class="btn btn-success btn-sm">Submit</button>
             </div>
         </form>
     </div>
